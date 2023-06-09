a824ca8637c4b3dc2bcf7dde2ae5be0388f5748e
tejas <tejasshah@gmail.com>
2023-06-09 15:41:16 +0530

    singleton

---
+++ src/main/java/com/App.java
@@ -0,0 +1,50 @@
+package com;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dao.UserDao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    
+
+        
+        UserDao userDao = new UserDao(); //10 times -> allocating -> gc 
+        userDao.add(); 
+        
+        //spring -> Ioc container -> object life cycle 
+        
+        //singleton design pattern 
+        
+        //IOC -> 
+        //1) BeanFactory
+        //2) ApplicationContext 
+        			// FileSystemXmlApplicationContext  {spring.xml --> project outside }
+			        // ClassPathXmlApplicationContext   {spring.xml --> class path } 
+			        // WebXmlApplicationContext 
+        			// anno 
+        
+        //spring -> 1)spring-core 2)spring-context 3)spring-el 4)spring-bean 
+    
+         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    
+         
+         UserDao userDao1 = context.getBean("userDao",UserDao.class);
+         UserDao userDao2 = context.getBean("userDao",UserDao.class);
+         UserDao userDao3 = context.getBean("userDao",UserDao.class);
+         
+         
+         System.out.println(userDao1);
+         System.out.println(userDao2);
+         System.out.println(userDao3);
+         
+    }
+    
+}
---
+++ src/main/java/com/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.dao;
+
+public class UserDao {
+
+	public void add() {
+		System.out.println("UserDao::add()");
+	}
+}
